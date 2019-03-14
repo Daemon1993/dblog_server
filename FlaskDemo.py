7ddfc100ec4351a6147ce95a858f323025d90693
liubo <965204390@qq.com>
2019-03-14 14:21:20 +0800

    a

---
+++ FlaskDemo.py
@@ -0,0 +1,20 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/get_pics')
+def getBlogPics():
+    r = requests.get('https://bing.getlove.cn/latelyBingImageStory')
+    return r.text
+
+
+if __name__ == '__main__':
+    app.run()
